698c40215e1dcb22c9ed829ec065767adee3d813
Ben Musoke-Lubega <benmusokelubega@princeton.edu>
2018-10-14 21:15:32 -0400

    Project Setup

---
+++ Reg.java
@@ -0,0 +1,12 @@
+//----------------------------------------------------------------------
+// Reg.java
+// Authors: Benjamin Musoke-Lubega (bpm3) Osita Ighodaro (ighodaro)
+//----------------------------------------------------------------------
+
+class Reg 
+{    public static void main(String[] args)
+    {
+        int address = args[0];
+        int port = args[1];
+    }
+}
